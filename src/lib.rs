90b71e30b16721118ebd65076b4ebad813f34722
Che <43485962+c-git@users.noreply.github.com>
2022-12-03 14:21:47 -0500

    Start filling out app structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 use chrono::NaiveDate;
 use clap::Parser;
 
+pub fn run(args: Cli) -> String {
+    todo!()
+}
+
 #[derive(Parser, Debug)]
 // #[command(author, version, about, long_about = None)]
 pub struct Cli {
     #[arg(short = 's', help = "Earliest date to include")]
     start_date: NaiveDate,
 
-    #[arg(short = 'e', help = "Last date to include")]
+    #[arg(short = 'e', help = "Last date to include (used as date of report)")]
     end_date: NaiveDate,
 
     #[arg(
@@ -15,5 +19,27 @@ pub struct Cli {
         default_value = "reports",
         help = "Path to top level folder that team folders are in"
     )]
-    folder: Option<String>,
+    folder: String,
+}
+
+struct InputFiles {
+    teams: Vec<TeamFiles>,
+}
+
+struct TeamFiles {
+    files: Vec<InputFile>,
+}
+
+struct InputFile {
+    date: NaiveDate,
+    member_name: String,
+    cancelled: Vec<Task>,
+    planned: Vec<Task>,
+    in_progress: Vec<Task>,
+    complete: Vec<Task>,
+}
+
+struct Task {
+    name: String,
+    comment: String, // TODO trim spaces
 }
--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     let args = Cli::parse();
 
     dbg!(args);
+
+    // TODO Load input files into memory
+
+    // TODO Generate output from input files
+
+    // TODO Save output to disk and report where file was saved
 }

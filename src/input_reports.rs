db7d25dfc2a549cda9659419cfb5d4248f810ea2
Che <43485962+c-git@users.noreply.github.com>
2022-12-04 00:35:58 -0500

    Implement iterating over team folders

--- src/input_reports.rs
+++ src/input_reports.rs
@@ -1,36 +1,71 @@
 use crate::settings::Cli;
+use anyhow::Context;
 use chrono::NaiveDate;
-use std::error::Error;
 use std::fs;
+use std::path::PathBuf;
 
-pub fn load_input_files(args: &Cli) -> Result<String, Box<dyn Error>> {
-    for file in fs::read_dir(&args.folder)? {
-        println!("{}", file?.path().display());
+pub fn load_input_files(args: &Cli) -> anyhow::Result<InputFiles> {
+    InputFiles::load_from_disk(args)
+}
+
+pub struct InputFiles {
+    pub teams: Vec<TeamFiles>,
+}
+
+impl InputFiles {
+    fn load_from_disk(args: &Cli) -> anyhow::Result<Self> {
+        let mut result = Self { teams: vec![] };
+
+        for file in fs::read_dir(&args.folder)
+            .with_context(|| format!("Failed to read dir: {}", &args.folder))?
+        {
+            // Iterating top level folder, expecting team folders only
+            let path = file?.path();
+            if path
+                .file_name()
+                .unwrap_or_else(|| panic!("Failed to get filename for {}", path.display()))
+                .to_string_lossy()
+                .starts_with('.')
+            {
+                println!("Skipping '{}' because it starts with a '.'", path.display());
+                continue;
+            }
+            if path.is_file() {
+                eprintln!(
+                    "Skipping '{}' because not expecting files at top level only team folders",
+                    path.display()
+                );
+                continue;
+            }
+            result.teams.push(TeamFiles::load_from_disk(path)?);
+        }
+        Ok(result)
     }
-    todo!()
 }
 
-struct InputFiles {
-    // TODO Ignore files that start with .
-    teams: Vec<TeamFiles>,
+pub struct TeamFiles {
+    pub name: String,
+    pub files: Vec<InputFile>,
 }
 
-struct TeamFiles {
-    files: Vec<InputFile>,
+impl TeamFiles {
+    fn load_from_disk(path: PathBuf) -> anyhow::Result<Self> {
+        todo!()
+    }
 }
 
-struct InputFile {
-    date: NaiveDate,
-    member_name: String,
-    is_team_lead: bool,
-    summary: String,
-    cancelled: Vec<Task>,
-    planned: Vec<Task>,
-    in_progress: Vec<Task>,
-    complete: Vec<Task>,
+pub struct InputFile {
+    pub date: NaiveDate,
+    pub member_name: String,
+    pub is_team_lead: bool,
+    pub summary: String,
+    pub cancelled: Vec<Task>,
+    pub planned: Vec<Task>,
+    pub in_progress: Vec<Task>,
+    pub complete: Vec<Task>,
 }
 
-struct Task {
-    name: String,
-    comment: String, // TODO trim spaces
+pub struct Task {
+    pub name: String,
+    pub comment: String, // TODO trim spaces
 }

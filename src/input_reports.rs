a9fc19bb60b7673d0f7f854c649b5f591cddd404
Che <43485962+c-git@users.noreply.github.com>
2022-12-04 01:36:54 -0500

    Implement the filtering of years and building vec

--- src/input_reports.rs
+++ src/input_reports.rs
@@ -1,7 +1,7 @@
 use crate::settings::Cli;
 use crate::utils::FileNameExtract;
 use anyhow::Context;
-use chrono::NaiveDate;
+use chrono::{Datelike, NaiveDate};
 use std::fs;
 use std::path::PathBuf;
 
@@ -33,7 +33,7 @@ impl InputFiles {
                 );
                 continue;
             }
-            result.teams.push(TeamFiles::load_from_disk(path)?);
+            result.teams.push(TeamFiles::load_from_disk(path, args)?);
         }
         Ok(result)
     }
@@ -45,8 +45,49 @@ pub struct TeamFiles {
 }
 
 impl TeamFiles {
-    fn load_from_disk(path: PathBuf) -> anyhow::Result<Self> {
-        todo!()
+    fn load_from_disk(dir_team: PathBuf, args: &Cli) -> anyhow::Result<Self> {
+        let mut result = Self {
+            name: dir_team.file_name_to_string_lossy().to_string(),
+            files: vec![],
+        };
+
+        let year_lower = args.start_date.year();
+        let year_upper = args.end_date.year();
+
+        for dir_year in fs::read_dir(&dir_team)
+            .with_context(|| format!("Failed to read dir: {}", &dir_team.display()))?
+        {
+            // Iterating year folders
+            let dir_year = dir_year?.path();
+            if dir_year.is_file() {
+                // TODO Add test to make sure this works
+                eprintln!(
+                    "Skipping '{}' because not expecting files at 2nd level only 4 digit year folders",
+                    dir_year.display()
+                );
+                continue;
+            }
+            let folder_year = dir_year.file_name_to_string_lossy().parse::<i32>();
+            match folder_year {
+                Ok(year) => {
+                    if year_lower <= year && year <= year_upper {
+                        result
+                            .files
+                            .push(InputFile::load_from_disk(dir_year, args)?);
+                    }
+                }
+                Err(e) => {
+                    // TODO Add test to make sure this works
+                    eprintln!(
+                        "Skipping '{}' because unable to parse '{}' into an integer with error: {}",
+                        dir_year.display(),
+                        &result.name,
+                        e
+                    );
+                }
+            }
+        }
+        Ok(result)
     }
 }
 
@@ -59,6 +100,12 @@ pub struct InputFile {
     pub planned: Vec<Task>,
     pub in_progress: Vec<Task>,
     pub complete: Vec<Task>,
+}
+
+impl InputFile {
+    fn load_from_disk(dir_year: PathBuf, args: &Cli) -> anyhow::Result<Self> {
+        todo!()
+    }
 }
 
 pub struct Task {

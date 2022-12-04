807bc54c68aa08037a48f0a7a025430db4fe1e5b
Che <43485962+c-git@users.noreply.github.com>
2022-12-04 00:35:14 -0500

    Change to anyhow
    The error messages look better

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::error::Error;
-
 use clap::Parser;
 use report_compiler::runner::{run, save_output};
 use report_compiler::settings::Cli;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
     let output = run(&args)?;
     let filename = save_output(&output, &args)?;
--- src/runner.rs
+++ src/runner.rs
@@ -1,16 +1,15 @@
-use crate::input_reports::load_input_files;
+use crate::input_reports::{load_input_files, InputFiles};
 use crate::settings::Cli;
-use std::error::Error;
 
-pub fn run(args: &Cli) -> Result<String, Box<dyn Error>> {
+pub fn run(args: &Cli) -> anyhow::Result<String> {
     let input = load_input_files(args)?;
     let output = generate_output(&input, args)?;
     Ok(output)
 }
 
-fn generate_output(input: &str, args: &Cli) -> Result<String, Box<dyn Error>> {
+fn generate_output(input: &InputFiles, args: &Cli) -> anyhow::Result<String> {
     todo!()
 }
-pub fn save_output(output: &str, args: &Cli) -> Result<String, Box<dyn Error>> {
+pub fn save_output(output: &str, args: &Cli) -> anyhow::Result<String> {
     todo!()
 }

c291556a8a7bd5aa192875290160594c63a0cdef
Che <43485962+c-git@users.noreply.github.com>
2022-12-03 19:46:39 -0500

    Implement run

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,18 @@ use std::error::Error;
 use std::fs;
 
 pub fn run(args: &Cli) -> Result<String, Box<dyn Error>> {
-    todo!();
-    // TODO Load input files into memory
+    let input = load_input_files(args)?;
+    let output = generate_output(&input, args)?;
+    Ok(output)
+}
 
-    // TODO Generate output from input files
+fn generate_output(input: &str, args: &Cli) -> Result<String, Box<dyn Error>> {
+    todo!()
+}
 
-    // TODO Save output to disk and report where file was saved
+fn load_input_files(args: &Cli) -> Result<String, Box<dyn Error>> {
     fs::read_dir("./change_this_path").unwrap();
-    Ok("".to_string())
+    todo!()
 }
 
 pub fn save_output(output: &str, args: &Cli) -> Result<String, Box<dyn Error>> {
